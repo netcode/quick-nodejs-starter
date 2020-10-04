54b9e81297365575dcc6b10c588734ac07cb7619
Eslam Salem <eslam@shieldfy.com>
2020-10-04 15:10:28 +0200

    add .env.example and some fixes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 require('dotenv').config();
 const debug = require('debug')('app:http');
 const express = require('express');
-
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const appURL = process.env.APP_URL || 'http://localhost';
+const serverPort = process.env.SERVER_PORT || 3000;
+
+//Main route
+app.get('/', (req,res) => {
+  return res.json({
+    "status":"success",
+    "message":"Hello World"
+  })
 });
 
-app.listen(port, () => {
-  debug(`Example app listening at http://localhost:${port}`);
+//Health Check endpoint
+app.get('/health', (req,res) => {
+  return res.json({
+    "status":"success",
+    "message":"OK"
+  })
+});
+
+
+//Listen
+app.listen(serverPort, () => {
+  debug(`${process.env.APP_NAME} listening at ${appURL}:${serverPort}`);
 });
